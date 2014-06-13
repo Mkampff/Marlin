1d85d4b512b7ea44a5de08c7c6856ed47994083c
eze-eoc <eai@eoconsulting.com.ar>
2014-06-13 15:40:59 -0300

    Changed the x, y and z speed when changing the filament.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -448,7 +448,9 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 // default settings
 
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {80 ,80 ,2560 ,630}  // default steps per unit for Ultimaker
-#define DEFAULT_MAX_FEEDRATE          {500, 500, 3, 25}    // (mm/sec)
+// BEGIN MODIF filament
+#define DEFAULT_MAX_FEEDRATE          {500, 500, 4, 45}    // (mm/sec)
+// END MODIF filament
 #define DEFAULT_MAX_ACCELERATION      {200,200,100,250}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
 #define DEFAULT_ACCELERATION          1000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
